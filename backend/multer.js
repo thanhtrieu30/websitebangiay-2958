62f435c4736a8053e8f80f41f749f51c5d7bddb8
Thanh Trieu <zpeng2k@gmail.com>
2023-06-19 12:18:53 +0700

    test upload

--- backend/multer.js
+++ backend/multer.js
@@ -1,14 +1,26 @@
 const multer = require("multer");
 const path = require("path");
 
+// const storage = multer.diskStorage({
+//   destination: function (req, res, cb) {
+//     cb(null, path.join(__dirname, "./uploads"));
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     const filename = file.originalname.split(".")[0];
+//     cb(null, filename + "-" + uniqueSuffix + ".png");
+//   },
+// });
+
 const storage = multer.diskStorage({
-  destination: function (req, res, cb) {
-    cb(null, path.join(__dirname, "./uploads"));
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const filename = file.originalname.split(".")[0];
-    cb(null, filename + "-" + uniqueSuffix + ".png");
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
   },
 });
 
