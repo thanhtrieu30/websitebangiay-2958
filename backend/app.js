6c88f22e442db813ac2e2233591ff62c7d51527e
Thanh Trieu <zpeng2k@gmail.com>
2023-06-25 11:54:28 +0700

    last update vercel

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/", express.static("./uploads"));
+app.use("/", express.static("uploads"));
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
