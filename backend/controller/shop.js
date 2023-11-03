4bce874990eb3f8f4c291e0b34845c902490cfbf
Thanh Trieu <zpeng2k@gmail.com>
2023-11-04 01:15:04 +0700

    updatee

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -44,8 +44,8 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://websitebangiay-2958.vercel.app/seller/activation/${activationToken}`;
-    // const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+    // const activationUrl = `https://websitebangiay-2958.vercel.app/seller/activation/${activationToken}`;
+    const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://websitebangiay-2958-apii.vercel.app/api/v2";
-export const backend_url = "https://websitebangiay-2958-apii.vercel.app/";
+// export const server = "https://websitebangiay-2958-apii.vercel.app/api/v2";
+// export const backend_url = "https://websitebangiay-2958-apii.vercel.app/";
 
-// export const server = "http://localhost:8000/api/v2";
-// export const backend_url = "http://localhost:8000/";
+export const server = "http://localhost:8000/api/v2";
+export const backend_url = "http://localhost:8000/";
